4c5edc7e06d351a0ee9e7dd58011aadd3ce2e682
Williams <nestleyoung87@gmail.com>
2025-05-30 00:29:57 +0100

    new push

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
         hostname = "app.terraform.io"
         organization = "Uzowilliams"
         workspaces {
-            name = "benco"
+            name = "bencoo"
         }
     }
 }
